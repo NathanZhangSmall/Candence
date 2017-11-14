17fb3369a0a416436b4034dcf1a04fac333a1f52
Nathan <616844758@qq.com>
2017-11-14 16:21:43 +0800

    初次提交allegro相关配置文件至Git

---
+++ skill/PCBdelV1_4.bat
@@ -0,0 +1,52 @@
+del /s PCBdelV1_3.bat
+del /s PCBdelV1_2.bat
+del /s PCBdelV1_1.bat
+
+rd /s /q signoise.run
+
+
+del /s *.log
+del /s eco.txt
+del /s pdf_out_config.txt
+del /s devices.dml
+del /s netrev.lst
+del /s master.tag
+del /s pxlBA.txt
+del /s *.form
+del /s *.clp
+del /s *.xml
+del /s "*.xml,*"
+del /s "*.txt,*"
+del /s *.dwl
+
+
+del /s allegro.jrl
+del /s "allegro.jrl,*"
+del /s "pad_designer.jrl"
+del /s "pad_designer.jrl,*"
+del /s "*.art,*"
+
+
+del /s "allegro.jrl,*"
+del /s "pstchip.dat,*"
+del /s "pstxnet.dat,*"
+del /s "pstxprt.dat,*"
+
+
+
+
+
+
+
+
+del /s *.drc
+del /s *.htm
+del /s *.html
+del /s "Status Report.txt"
+rd /s /q History
+del /s "*.~br"
+del /s "*.~pr"
+del /s *.PrjPcbStructure
+
+
+
